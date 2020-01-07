75d4c8fbb12b2e856c092a93744a1ec9d04db0a4
BsNoSi <github@buoa.de>
2020-01-07 15:00:17 +0100

    Update v1.6.0
    - Filename instead of timestamp as ID 
    - text file for *large* title, 
    - default *small* title
    - Readme overhaul

--- imgpop.php
+++ imgpop.php
@@ -7,52 +7,54 @@
 
 class YellowImgPop
 {
-	const Version = "1.5.0";
+	const Version = "1.6.0";
 	var $yellow;			//access to API
-	// Handle initialisation
-	function onLoad($yellow)
-	{
+// Handle initialisation
+	function onLoad($yellow) {
 		$this->yellow = $yellow;
 	}
-	// Handle page content parsing of custom block
+// Handle page content parsing of custom block
 	public function onParseContentShortcut($page, $name, $text, $type) {
 		$output = NULL;
 		if($name=="imgpop") {
 			list($TheImage, $TheTitle, $TheID, $TheClass) = $this->yellow->toolbox->getTextArgs($text);
-					
+			
 			if(empty($text)) {
 				$output = '<b>[imgpop (/media/image/)TheImage TheTitle TheID TheClass]</b>';  
 			}
-		else {	
-			$TheID = $TheID ? : time();
-			$TheClass = (!$TheClass) ? $TheClass = ' class = "ipop"' : $TheClass = ' class="' . $TheClass . '"';
-			
-			$TheImage = $this->yellow->system->get("imageDir").$TheImage;
-			
-			if(empty($TheTitle)){
-				$exif = @exif_read_data($TheImage, 'COMMENT',true,false);
-				$TheTitle = utf8_encode($exif['COMMENT'][0]);
-		    }
-		    if(empty($TheTitle)) $TheTitle = $this->yellow->text->get("imgpop_NoTitle");
-			// $TheTitle = strip_tags($TheTitle,'<br>');
-		 	$tip = strip_tags($TheTitle);
-	
-			$output = '<div' . $TheClass . '>';
-			$output .= '<a class="iboxx" href="#' . $TheID . '"><img src="/' . $TheImage . '" title="'.$tip.'" /><span class="imag">🔍</span></a>';
-			$output .= '<a class="imgbox" id="' . $TheID . '"href="#_"><img src="/' . $TheImage . '"><span class="imgboxtitle">'.$tip.'</span></a></div>';		
+			else {
+				$TheID = $TheID ? : pathinfo($TheImage)["filename"];
+				$TheClass = (!$TheClass) ? $TheClass = ' class = "ipop"' : $TheClass = ' class="' . $TheClass . ' ipop"';
+				$TheImage = $this->yellow->system->get("imageDir").$TheImage;
+				
+				if(empty($TheTitle)) {
+					$check = $this->yellow->system->get("imageDir").pathinfo($TheImage)["filename"] . ".txt";
+					if (file_exists($check)) {
+						$TheTitle = file_get_contents($check);
+					}
+					else {
+						$exif = @exif_read_data($TheImage, 'COMMENT',true,false);
+						$TheTitle = utf8_encode($exif['COMMENT'][0]);
+					}
+				}
+				if(empty($TheTitle)) $TheTitle = $this->yellow->text->get("imgpop_NoTitle");
+				$tip = strip_tags($TheTitle,"<br>,<br/>");
+				$output = '<div' . $TheClass . '>';
+				$output .= '<a class="iboxx" href="#' . $TheID . '"><img src="/' . $TheImage . '" title="'.$this->yellow->text->get("imgpop_zoom").'" /><span class="imag">🔍</span></a>';
+				$output .= '<a class="imgbox" id="' . $TheID . '"href="#_"><img src="/' . $TheImage . '"><span class="imgboxtitle">'.$tip.'</span></a></div>';
 			}
 		}
 		return $output;
-		}
+	}
 		
-		// Handle page extra data
-		public function onParsePageExtra($page, $name) {
-        $output = null;
-        if ($name=="footer") {
-            $extensionLocation = $this->yellow->system->get("serverBase").$this->yellow->system->get("extensionLocation");
-            $output = "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}imgpop.css\" />\n";
-        }
-        return $output;
+// Handle page extra data
+	public function onParsePageExtra($page, $name) {
+		$output = null;
+		if ($name=="footer") {
+			$extensionLocation = $this->yellow->system->get("serverBase").$this->yellow->system->get("extensionLocation");
+			$output = "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}imgpop.css\" />\n";
+		}
+		return $output;
 	}
 }
 ?>
